ee768d1e579c257c9c5d7f7243d0c1c9a3cf5916
sakunika <muthunihansani052@gmail.com>
2024-11-08 17:47:31 +0530

    Form and Email Validation

--- lib/Comm/comHelper.dart
+++ lib/Comm/comHelper.dart
@@ -12,3 +12,9 @@ void alertDialog(String message) {
     fontSize: 16.0,
   );
 }
+
+bool validateEmail(String email){
+  final RegExp emailReg = new RegExp(
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+  return emailReg.hasMatch(email);
+}
